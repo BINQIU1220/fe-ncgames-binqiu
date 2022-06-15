919d487a5d57839cfabbd8b6b2302fd4cd945cc3
BINQIU1220 <ezio12a@hotmail.com>
2022-06-15 16:02:34 +0100

    limited vote click per page load

--- src/components/PatchVotes.jsx
+++ src/components/PatchVotes.jsx
@@ -4,38 +4,46 @@ import { useNavigate } from "react-router-dom";
 import { GoThumbsup, GoThumbsdown } from "react-icons/go";
 
 const PatchVotes = ({ previsouVotes, review_id }) => {
-    const [votes, setVotes] = useState(previsouVotes)
-    const [err, setErr] = useState(null);
-    const navigate = useNavigate();
+  const [votes, setVotes] = useState(previsouVotes);
+  const [isDisbled, setIsDisabled] = useState(false);
+  const [err, setErr] = useState(null);
+  const navigate = useNavigate();
 
-    const thumbUp = () => {
-        setVotes((currentVotes) => currentVotes + 1);
-        setErr(null);
-        patchVotesById(1, review_id).catch((err) => {
-            setVotes((currentVotes) => currentVotes - 1);
-            setErr('Something went wrong, please try again.');
-            navigate("/othererrors")
-        });
-    };
+  const thumbUp = () => {
+    setIsDisabled(true);
+    setVotes((currentVotes) => currentVotes + 1);
+    setErr(null);
+    patchVotesById(1, review_id).catch((err) => {
+      setVotes((currentVotes) => currentVotes - 1);
+      setErr("Something went wrong, please try again.");
+      navigate("/othererrors");
+    });
+  };
 
-    const thumbDown = () => {
-        setVotes((currentVotes) => currentVotes - 1);
-        setErr(null);
-        patchVotesById(-1, review_id).catch(() => {
-            setVotes((currentVotes) => currentVotes + 1);
-            setErr('Something went wrong, please try again.');
-            navigate("/othererrors")
-        });
-    };
+  const thumbDown = () => {
+    setIsDisabled(true);
+    setVotes((currentVotes) => currentVotes - 1);
+    setErr(null);
+    patchVotesById(-1, review_id).catch(() => {
+      setVotes((currentVotes) => currentVotes + 1);
+      setErr("Something went wrong, please try again.");
+      navigate("/othererrors");
+    });
+  };
 
-    return (
-        <div id="vote-icons">
-            <GoThumbsup id="thumb-up" onClick={thumbUp} />
-            <GoThumbsdown id="thumb-down" onClick={thumbDown} />
-            <p id="single-review-votes">Total votes: {votes}</p>
-        </div>
-    )
-
-}
+  return (
+    <>
+      <p id="single-review-votes">Total votes: {votes}</p>
+      <div id="vote-icons">
+        <button id="thumb-up" disabled={isDisbled} onClick={thumbUp}>
+          <GoThumbsup />
+        </button>
+        <button id="thumb-down" disabled={isDisbled} onClick={thumbDown}>
+          <GoThumbsdown />
+        </button>
+      </div>
+    </>
+  );
+};
 
 export default PatchVotes;
