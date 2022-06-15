a6e174a39e1379afdbe8c26cbb38c20ec77a4734
BINQIU1220 <ezio12a@hotmail.com>
2022-06-15 15:27:44 +0100

    add back-home button to erro page

--- src/components/ErroHandle.jsx
+++ src/components/ErroHandle.jsx
@@ -1,7 +1,31 @@
+import { AiFillHome } from "react-icons/ai"
+import { useNavigate } from "react-router-dom";
+import "../styling/ErrorHandle.css";
+
 const ErroHandle = ({ error }) => {
-    console.log(error, "<<<error in errorhandle")
-    if (error === 404) return <img className="404" src="https://i.ibb.co/h7zxBwy/404.png" alt="Error 404 image"></img>
-    else if (error = 'Something went wrong, please try again.') return <img className="othererrors" src="https://i.pinimg.com/originals/b8/b8/f7/b8b8f787c454cf1ded2d9d870707ed96.png" alt="Error others image"></img>
-}
+    const navigate = useNavigate();
+
+    if (error === 404) {
+        return (
+            <div className="error-404">
+                <img
+                    className="error-404"
+                    src="https://i.ibb.co/h7zxBwy/404.png"
+                    alt="Error 404 image"
+                ></img>
+                <button className="back-home-btn-errorpage" onClick={() => { navigate("/") }}><AiFillHome /> Back Home</button>
+            </div>
+        );
+    } else if ((error = "Something went wrong, please try again."))
+        return (
+            <div className="error-others">
+                <img
+                    className="othererrors"
+                    src="https://i.pinimg.com/originals/b8/b8/f7/b8b8f787c454cf1ded2d9d870707ed96.png"
+                    alt="Error others image"
+                ></img> <button className="back-home-btn-errorpage" onClick={() => { navigate("/") }}><AiFillHome /> Back Home</button>
+            </div>
+        );
+};
 
 export default ErroHandle;
